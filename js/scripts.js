eb0e82fefbdc9d22422f123805864099bc324d42
Nabil Pervez <perveznabil@gmail.com>
2020-09-19 11:08:40 -0500

    week 4 assignment

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,12 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause"); 
         }
     });
+    //click reserve button > reserve modal toggled
+    $("#reserveButton").click(function(){
+        $('#reserveModal').modal('toggle')
+    })
+    //click login button > login modal toggled
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('toggle')
+    })
 });
